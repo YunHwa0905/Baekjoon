e42ea22b7f258ca3fe3c62f867a7df96dae99323
YunHwa0905 <yoho030905@gmail.com>
2026-04-02 12:27:01 +0900

    [Bronze V] Title: Correct, Time: 172 ms, Memory: 17744 KB -BaekjoonHub

---
+++ Java/백준/Bronze/26307. Correct/Correct.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int HH = sc.nextInt();
+        int MM = sc.nextInt();
+        int result = 0;
+        
+        result += (HH-9)*60 + MM;
+        
+        System.out.println(result);
+    }
+}
